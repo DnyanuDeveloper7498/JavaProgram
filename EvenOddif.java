3cce712f8b46989180bacdda864fd1ebf4271e0c
Dnyaneshwar Rathod <151998044+DnyanuDeveloper7498@users.noreply.github.com>
2026-02-02 22:10:50 +0530

    Update EvenOddif.java

--- EvenOddif.java
+++ EvenOddif.java
@@ -3,7 +3,6 @@
 
 import java.util.Scanner;
 
-
 class EvenOddif 
 {
 	public static void main(String[] args) 
@@ -13,18 +12,8 @@ class EvenOddif
 		
 		System.out.println("Eneter Number:-");
 		int num =sc.nextInt();
-		
-		System.out.println("Eneter a Char :-");
-		
-		char ch =sc.next().charAt(0);
-		
-		
-		
 	
-		if (num==0)
-		 { 
-			 System.out.println("Plese Eneter any number");
-		 } else if (num%2==0)
+		  if (num%2==0)
 		{
 			System.out.println(num + " num is Even");
 		}
@@ -34,18 +23,7 @@ class EvenOddif
 			 System.out.println(num + "Num is odd");
 			
 		}
-		 
-		 if (ch=='a'||ch=='A'|| ch=='E'||ch=='e'||  ch=='I'||ch=='i'|| ch=='O'||ch=='o'||ch=='U'||ch=='u')
-		 {
-			 
-			 System.out.println( ch + " char is Vowel ");
-		 }
-		 else 
-		{
-			  System.out.println( ch + " char is not a Vowel"); 
-		}
-		
-		
+		 	
 		
 	} 
 }
